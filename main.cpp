fb68bb26e0c6b31419c7fe25ab0678aef919c1f6
FujitaKentaro <k021g1261@g.neec.ac.jp>
2023-10-02 14:00:11 +0900

    2-3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
-	printf("Hello,World!\n");
+int main(int argc,char* argv[]) {
+	// argvの数だけ繰り返す
+	for (int i = 0; i < argc; i++) {
+		// 文字列argvのi番を表示
+		printf(argv[i]);
+		// 改行
+		printf("\n");
+	}
 
 	system("pause");
 	return 0;
