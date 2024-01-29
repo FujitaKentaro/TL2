7e4a908deed30b5e2a5be1cd84ec855c8b02549b
FujitaKentaro <k021g1261@g.neec.ac.jp>
2024-01-29 10:58:29 +0900

    o

--- main.cpp
+++ main.cpp
@@ -14,20 +14,39 @@ enum Argument {
 
 int main(int argc, char* argv[]) {
 
-	assert(argc >= NumArgument);
+	//for (int i = 0; i < argc; i++) {
+	//	printf(argv[i]);
+	//	//改行
+	//	printf("\n");
+	//}
+
+	/*assert(argc >= NumArgument);*/
+
+	//コマンドライン引数指定なし
+	if (argc < NumArgument)
+	{
+		//使い方を表示する
+		TextureConverter::OutputUsage();
+		return 0;
+	}
 
 	//ライブラリの初期化
 	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 	assert(SUCCEEDED(hr));
 
+	//オプションの数
+	int numOptions = argc - NumArgument;
+	//オプション配列(ダブルポインタ)
+	char** options = argv + NumArgument;
+
 	//テクスチャコンバータ
 	TextureConverter converter;
 
-	converter.ConverterTextureWICToDDS(argv[kFilePath]);
+	converter.ConverterTextureWICToDDS(argv[kFilePath], numOptions, options);
 
 	//COMライブラリの終了
 	CoUninitialize();
 
-	//system("pause");
+	/*system("pause");*/
 	return 0;
 }
